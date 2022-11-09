e6d80e7807fdf186afd2e92afad41e1de1e83256
fpmovec <10.maks.10K@gmail.com>
2022-11-09 11:00:51 +0300

    Added Tests

---
+++ TestsStrings/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestsStrings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestsStrings")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f3755d40-3451-4785-aade-87c2078d7283")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestsStrings/UnitTest1.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Xunit;
+using System.Reflection.Emit;
+using SemanticComparison;
+using RESTful_Service.Models;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTestOne
+    {
+        [TestMethod]
+        public void ConcateTestMethodOne()
+        {
+            string stringOne = "Anton ";
+            string stringTwo = "Semenchenko";
+            string expected = "Anton Semenchenko";
+            Strings strings = new Strings();
+            strings._stringOne = stringOne;
+            strings._stringTwo = stringTwo;
+
+            string actual = strings.StringsConcat();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ConcateTestMethodTwo()
+        {
+            string stringOne = "I love ";
+            string stringTwo = "C#";
+            string expected = "I love C#";
+            Strings strings = new Strings();
+            strings._stringOne = stringOne;
+            strings._stringTwo = stringTwo;
+
+            string actual = strings.StringsConcat();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReplaysTestMethodOne()
+        {
+            string stringOne = "C#";
+            int replays = 3;
+            string expected = "C#C#C#";
+            Strings strings = new Strings();
+            strings._stringOne = stringOne;
+            strings._replaysCount = replays;
+
+            string actual = strings.StringsReplay();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ReplaysTestMethodTwo()
+        {
+            string stringOne = null;
+            Strings strings = new Strings();
+            strings._stringOne = stringOne;
+
+            Assert.ThrowsException<ArgumentNullException>(() => strings.StringsReplay());
+        }
+    }
+}
